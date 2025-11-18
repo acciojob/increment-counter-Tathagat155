b3fa6050dd1b216f947c9a9d5829564536ee5b47
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-18 14:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ let p=document.getElementById("counter");
+  let button=document.getElementById("btn");
+  p.addEventListener("click",()=>{
+	  let incrementValue=parseInt(p.textContent);
+	  alert(incrementValue);
+	  p.textContent=incrementValue+1;
+  });
